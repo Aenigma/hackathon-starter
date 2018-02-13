fd2d0b6f62225bd0406b8df3bcdd73861a5b2c98
Kevin Raoofi <nullindev@gmail.com>
2018-02-12 20:52:52 -0500

    added views

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const app = express()
 
 const PORT = process.env.PORT || 3000;
 
+app.set('view engine', 'ejs');
+
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.render('index', {
+        data: ['foo', 'bar', 'baz']
+    })
 })
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))
